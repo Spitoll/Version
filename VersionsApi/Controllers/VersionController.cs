4c59b39bb459616c35db07dc87e73963c065ea5f
spitoll <oleksandr.petrenko@advantiss.com>
2022-05-06 18:44:17 +0300

    1

---
+++ VersionsApi/Controllers/VersionController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VersionsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return GetType().Assembly.GetName().Version.ToString();
+        }
+    }
+}
